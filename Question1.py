801653fba5a2ea7be22b1f5fbf498b958a735483
Daniel Stevanus <alatgt88@gmail.com>
2024-03-13 22:05:08 -0400

    13/2

--- Question1.py
+++ Question1.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def home():
     return "Hello World!"
 
+@app.route("/<name>", methods = ["GET"])
+def process_name(name):
+    if name.isupper() and name.isalpha():
+        greet = name.lower()
+    elif name.islower() and name.isalpha():
+        greet = name.upper()
+    elif not name.isalpha():    
+        greet = ''
+        for char in name:
+            if char.isalpha():
+                greet = greet + char
+    else:
+            greet = name[:1].upper() + name[1:].lower()
+    return "<html><body><h1>Welcome, "+ greet+ " to my CSCB20 website!</h1></body></html>"
+
 if __name__ == "__main__":
     app.run(debug=True)
