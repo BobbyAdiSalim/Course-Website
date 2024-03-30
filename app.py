1bba15f73b4171770d85943547d5c831450f19f6
Daniel Stevanus <alatgt88@gmail.com>
2024-03-29 21:59:50 -0400

    Template almost done. progress 50%

--- app.py
+++ app.py
@@ -6,5 +6,40 @@ app = Flask(__name__, template_folder="src")
 def home():
     return render_template("index.html")
 
+@app.route("/labs")
+def labs():
+    return render_template("labs.html")
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
+
+@app.route("/lectures")
+def lectures():
+    return render_template("lectures.html")
+
+@app.route("/assignments")
+def assignments():
+    return render_template("assignments.html")
+
+@app.route("/tests")
+def tests():
+    return render_template("tests.html")
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+@app.route("/resources")
+def resources():
+    return render_template("resources.html")
+
+@app.route("/feedback")
+def feedback():
+    return render_template("feedback.html")
 if __name__ == "__main__":
     app.run(debug=True)
