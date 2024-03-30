d6daf03e0a6b00c7f0ffed7ac5c15a2cc296a462
Daniel Stevanus <alatgt88@gmail.com>
2024-03-30 17:41:32 -0400

    db sudah jadi

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///assignment3.db'
+
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True, primary_key = True, nullable = False)
+    password = db.Column(db.Text, nullable = False)
+    name = db.Column(db.String(250), nullable = False)
+    date_created = db.Column(db.Date)
+    grades = db.relationship("Grades")
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(250), nullable = False)
+    desc = db.Column(db.Text)
+    due_date = db.Column(db.Date)
+    grades = db.relationship("Grades")
+
+class Grades(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable = False)
+    test_id = db.Column(db.Integer, db.ForeignKey('test.id'), nullable = False)
+
+class Instructor(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True, primary_key = True, nullable = False)
+    password = db.Column(db.Text, nullable = False)
+    name = db.Column(db.String(250), nullable = False)
+    date_created = db.Column(db.Date)
+
 
 @app.route("/")
 def home():
@@ -42,5 +75,13 @@ def resources():
 def feedback():
     return render_template("feedback.html", feedback = True)
 
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# app.app_context().push()
+# student1 = Student(... = ....)
+# db.session.add(student1)
+# db.session.commit()
