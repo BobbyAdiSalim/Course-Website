02960351d2fd2aa00772dfcb70f55ae215dc1f08
Daniel Stevanus <alatgt88@gmail.com>
2024-03-30 21:03:37 -0400

    sudah bisa jd instructor

--- app.py
+++ app.py
@@ -112,11 +112,17 @@ def addtest():
 def view():
     if("auth" in session and session["auth"] == 'instructor'):
         students = Student.query.all()
-        return render_template("grades.html", students = students)
+        return render_template("modify_grades.html", students = students)
     grades = None
     # grades = Grades.query.filter_by(username = session["username"]).get()
     tests = Test.query.all()
     return render_template("grades.html", tests = tests, grades = grades)
+
+
+@app.route('/loginInstructor')
+def login_instructor():
+    session["auth"] = "instructor"
+    return "You are instructor now, love you"
 
 if __name__ == "__main__":
     app.run(debug=True)
