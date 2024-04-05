7f7a81c1789228a2680fbc65878cd356c964148c
BobbyAdiSalim <13obby1025@gmail.com>
2024-04-05 15:52:35 -0400

    ENTER BISA PINDAH INPUT

---
+++ static/js/update_grades.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.grade-input').keypress(function (e) {
+        if (e.keyCode === 13) {
+            var currentIndex = $('.grade-input').index(this);
+            $('.grade-input').eq(currentIndex + 1).focus();
+            e.preventDefault();
+        }
+    });
+  });
